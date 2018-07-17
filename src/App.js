feb52959fb455c871e8dab2a1312abd6459a7c8e
versayr <themoose3@gmail.com>
2018-07-17 13:55:30 -0400

    Add input

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import { 
+  Paper, 
+  AppBar,
+  Typography,
+  Button, 
+  TextField
+} from '@material-ui/core';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Paper>
+          <AppBar position="static">
+            <Typography 
+              variant="display1" 
+              color="inherit"
+              gutterBottom
+            >
+              Check.ly
+            </Typography>
+            <Typography 
+              variant="subheading" 
+              color="inherit"
+              gutterBottom
+            >
+              The Material-UI Mockup
+            </Typography>
+          </AppBar>
+          <form>
+            <TextField
+              autoFocus="true"
+            />
+            <Button
+              type="submit"
+              color="primary"
+              variant="raised"
+            >
+              Add Task
+            </Button>
+          </form>
+        </Paper>
+      </div>
+    );
+  }
+}
+
+export default App;
