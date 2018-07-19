74f10ddb72636b9ee90cffced64900fc32eab5b3
versayr <themoose3@gmail.com>
2018-07-19 17:10:06 -0400

    Add list

--- src/App.js
+++ src/App.js
@@ -9,8 +9,19 @@ import {
   TextField
 } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: ['first task', 'second task']
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -39,10 +50,19 @@ class App extends Component {
               type="submit"
               color="primary"
               variant="fab"
+              mini
             >
               <AddIcon />
             </Button>
           </form>
+          <List>
+            <ListItem button>
+              <ListItemText primary={this.state.todo[0]} />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary={this.state.todo[1]} />
+            </ListItem>
+          </List>
         </Paper>
       </div>
     );
