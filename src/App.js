409c3cf1da88c4121403cd317cacc1022e37dcef
versayr <themoose3@gmail.com>
2018-07-19 21:47:51 -0400

    Add theme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import './App.css';
-
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { 
   Paper, 
   AppBar,
   Typography,
   Button, 
-  TextField
+  TextField,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText
 } from '@material-ui/core';
-import AddIcon from '@material-ui/icons/Add';
-import DeleteIcon from '@material-ui/icons/Delete';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import './App.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#02B087'
+    },
+  },
+});
 
 class App extends Component {
   constructor(props) {
@@ -24,47 +33,48 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Paper>
-          <AppBar position="static">
-            <Typography 
-              variant="display1" 
-              color="inherit"
-              gutterBottom
-            >
-              Check.ly
-            </Typography>
-            <Typography 
-              variant="subheading" 
-              color="inherit"
-              gutterBottom
-            >
-              The Material-UI Mockup
-            </Typography>
-          </AppBar>
-          <form>
-            <TextField
-              autoFocus="true"
-            />
-            <Button
-              type="submit"
-              color="primary"
-              variant="fab"
-              mini
-            >
-              <AddIcon />
-            </Button>
-          </form>
-          <List>
-            <ListItem button>
-              <ListItemText primary={this.state.todo[0]} />
-            </ListItem>
-            <ListItem button>
-              <ListItemText primary={this.state.todo[1]} />
-            </ListItem>
-          </List>
-        </Paper>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Paper>
+            <AppBar position="static">
+              <Typography 
+                variant="display1" 
+                color="inherit"
+                gutterBottom
+              >
+                Check.ly
+              </Typography>
+            </AppBar>
+            <form>
+              <TextField
+                autoFocus="true"
+              />
+              <Button
+                type="submit"
+                color="primary"
+                variant="fab"
+                mini
+              >
+                <AddIcon />
+              </Button>
+            </form>
+            <List>
+              <ListItem button>
+                <ListItemText primary={this.state.todo[0]} />
+                <ListItemIcon>
+                  <DeleteIcon />
+                </ListItemIcon>
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary={this.state.todo[1]} />
+                <ListItemIcon>
+                  <DeleteIcon />
+                </ListItemIcon>
+              </ListItem>
+            </List>
+          </Paper>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/components/theme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const Theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#02B087'
+    },
+  },
+});
+
+export default Theme;
